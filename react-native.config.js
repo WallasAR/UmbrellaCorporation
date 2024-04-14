8accfe6890692c3464a8a800999e6eee9db1a121
WallasAR <rochaaguiar2016@gmail.com>
2024-04-14 19:22:14 -0300

    Adding requirements
    - Adding fonts;
    - Adding files to reference colors and fonts that will be used.

---
+++ react-native.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    project: {
+        ios: {},
+        andoid: {},
+    },
+    assets: ['./src/assets/fonts'],
+  };    
---
+++ src/styles/theme.ts
@@ -0,0 +1,69 @@
+export default {
+  COLORS: {
+    // Icones de navegação, screenBackground, googleButton, ...
+    WHITE: "#FFF",
+
+    // Inputs
+    GRAY: "#F2F2F2",
+
+    // Borda quadro estela
+    GRAY2: "#EDEDED",
+
+    // Seleção medicamento
+    GRAY3: "#DBDBDB",
+
+    // InputIcons, Texto descrição Input avaliação
+    GRAY4: "#BDBDBD",
+    
+    // Estela não preenchida (somente borda)
+    GRAY5: "#C4C5C4",
+
+    // Texto tempo da avaliação 
+    GRAY6: "#838589",
+
+    // Texto detalhamento de informações pedidos
+    GRAY7: "#515151", 
+
+    // navBackgound, gradient
+    RED1: "#F74838",
+
+    // Buttons
+    RED2: "#F53827",
+
+    // GradientUmbrellaCorp Text
+    RED3: "#912A21",
+
+    // IconConfig, TextConfig
+    RED4: "#611811",
+
+    // SocialFacebook
+    BLUE: "#4368C7",
+
+    // starFilled
+    YELLOW: "#FFC120",
+
+    // Descrição Carrinho
+    YELLOW2: "#FEFFDC",
+
+    // Button
+    GREEN: "#48BD00",
+
+    // okIcon
+    GREEN2: "#56BE15",
+
+    // Texts
+    BLACK: "#000"
+    
+  },
+
+  FONTS: {
+    TEXT: "DMSans-Medium",
+    TITLE: "Rubik-Medium",
+    
+    QUICKSANDLIGHT: "Quicksand-Light",
+    QUICKSANDMEDIUM: "Quicksand-Medium",
+    QUICKSANDREGULAR: "Quicksand-Regular",
+    QUICKSANDBOLD: "Quicksand-Bold",
+    QUICKSANDSEMIBOLD: "Quicksand-SemiBold",
+  },
+};
